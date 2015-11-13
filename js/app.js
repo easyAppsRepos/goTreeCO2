02f29e84b40282f50e423c1de242602eef01e0aa
root <root@localhost.localdomain>
2015-11-13 16:10:36 -0600

    frontend beta V0.1

---
+++ js/app.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function() {
+
+    $JSView.run('bottom');
+    $JSView.declareView({ 
+
+ 	menuTabs: {
+            url: '/menuTabs',
+            template: 'views/menuTabs.html',
+            controller: 'menuTabs'
+        },
+        viewA: {
+            url: '/viewA',
+            template: 'views/viewA.html',
+            controller: 'viewA'
+        },
+        viewB: {
+            url: '/viewB',
+            template: 'views/viewB.html',
+            controller: 'viewB'
+        },
+        viewC: {
+            url: '/viewC',
+            template: 'views/viewC.html',
+            controller: 'viewC'
+        }
+    });
+     
+    /*Declare modal*/
+    $JSView.declareModal({
+        modalA: {
+            url: '/modalA',
+            template: 'views/modalA.html',
+            controller: 'modalA'
+        }
+    });
+     
+    /*Asign view start*/
+    $JSView
+        .initView('menuTabs');
+ 
+}, false);
---
+++ js/controllers.js
@@ -0,0 +1,21 @@
+$JSView.controller = {
+    menuTabs:function(e){
+
+        $JSView.dataView({
+            'title': 'Tabs'
+        },e)
+        $JSView.initTabs(e);
+    },
+    viewA: function(e){
+		$JSView.dataView({},e)
+	},
+    viewB: function(e){
+		$JSView.dataView({},e)	
+	},
+    viewC: function(e){
+		$JSView.dataView({},e)
+	},
+    modalA: function(e){
+		$JSView.dataView({},e)	
+	}
+}
