90c727dd2ed073e50edbfa10d32c323ea500c0da
root <root@localhost.localdomain>
2015-11-17 18:33:26 -0600

    qrTest

--- js/app.js
+++ js/app.js
@@ -184,3 +184,19 @@ function loginButton(){
 	}catch(e){alert('error de conexion catch'+e);
 	} 
 }
+
+function getQR(){
+
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+
+}
